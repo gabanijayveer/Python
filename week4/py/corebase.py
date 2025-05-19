5ffaa4ab4c0bf71a30a2ebdb87f115104527f7de
gabanijayveer <jayveerjnext@gmail.com>
2025-05-19 18:04:10 +0530

    corebase file

--- week4/py/corebase.py
+++ week4/py/corebase.py
@@ -4,9 +4,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-
 # -----------------------------
 # Feedforward Neural Network
 # -----------------------------
